3682072a89de51c378ddf16b59765781c67cda4e
Michael Lydeamore <michael.lydeamore@adelaide.edu.au>
2014-07-30 16:57:43 +1000

    Basic multiline handling, still requires padding

--- src/main/java/com/insane/simpleachievements/client/gui/ButtonCheckBox.java
+++ src/main/java/com/insane/simpleachievements/client/gui/ButtonCheckBox.java
@@ -10,17 +10,22 @@ import org.lwjgl.opengl.GL11;
 import com.insane.simpleachievements.AchievementHandler.SimpleAchievement;
 import com.insane.simpleachievements.SimpleAchievements;
 
+import java.util.ArrayList;
+
 public class ButtonCheckBox extends GuiButton
 {
 	private static final ResourceLocation texture = new ResourceLocation(SimpleAchievements.MODID.toLowerCase(), "textures/gui/checkboxes.png");
 	
 	private final SimpleAchievement achievement;
+
+    private static int maxStringLength=28;
 	
 	public ButtonCheckBox(int id, int x, int y, int width, int height, SimpleAchievement achievement, GuiScreen parent)
 	{
 		super(id, x, y, width, height, achievement.text);
 		
 		this.achievement = achievement;
+
 	}
 	
 	@Override
@@ -43,11 +48,51 @@ public class ButtonCheckBox extends GuiButton
 		
 		drawTexturedModalRect(xPosition, yPosition + ((height - 20) / 2), offsetX, offsetY, 20, 20);
 
-		par1Minecraft.fontRenderer.drawSplitString(this.displayString, xPosition + 25, yPosition + (height / 2) - 4, this.width, achievement.getState() ? 0x009105 : 0x000000);
+        ArrayList<String> lines = getExpectedLines(achievement.text);
+        switch (lines.size()) {
+            case 1:
+                par1Minecraft.fontRenderer.drawString(this.displayString, xPosition + 25, yPosition + (height / 2) - 4, achievement.getState() ? 0x009105 : 0x000000, false);
+                break;
+            case 2:
+                par1Minecraft.fontRenderer.drawString(lines.get(0), xPosition + 25, yPosition + (height / 3) - 4, achievement.getState() ? 0x009105 : 0x000000, false);
+                par1Minecraft.fontRenderer.drawString(lines.get(1), xPosition + 25, yPosition + 2*(height / 3) - 4, achievement.getState() ? 0x009105 : 0x000000, false);
+                break;
+            case 3:
+                par1Minecraft.fontRenderer.drawString(lines.get(0), xPosition + 25, yPosition + (height / 3) - 6, achievement.getState() ? 0x009105 : 0x000000, false);
+                par1Minecraft.fontRenderer.drawString(lines.get(1), xPosition + 25, yPosition + 2*(height / 3) - 6, achievement.getState() ? 0x009105 : 0x000000, false);
+                par1Minecraft.fontRenderer.drawString(lines.get(2), xPosition + 25, yPosition + 3*(height / 3) - 6, achievement.getState() ? 0x009105 : 0x000000, false);
+                break;
+            default:
+                System.out.println(lines);
+                par1Minecraft.fontRenderer.drawSplitString(this.displayString, xPosition + 25, yPosition + (height / 2) - 4, this.width, achievement.getState() ? 0x009105 : 0x000000);
+        }
 	}
 
 	public int getHeight()
 	{
 		return this.height;
 	}
+
+    public static ArrayList<String> getExpectedLines(String str) {
+        String text = str;
+        int numLines = (text.length()/maxStringLength)+1;
+
+        int counter=0;
+        ArrayList<String> result = new ArrayList<String>();
+        String[] wordArray = text.split(" ");
+        String current= "";
+        for (int i=0; i<wordArray.length; i++) {
+            if ((current+wordArray[i]).length()<maxStringLength) {
+                current = current + " " + wordArray[i];
+            } else {
+                result.add(current);
+                current=wordArray[i];
+            }
+        }
+        if (!current.equals("")) {
+            result.add(current);
+        }
+
+        return result;
+    }
 }
